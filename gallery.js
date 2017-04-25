603100497af1d328f13974a03b076681fe308cd0
Unknown <cwilson66@mail.csuchico.edu>
2017-04-25 12:39:00 -0700

    Finished Part 1

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
+var chico = [];
+chico = ["Upper Bidwell", "One Mile", "Naked Lounge", "Dnook", "Woodstocks"];
+var current;
 
 function shuffleGallery()
 {
-
+  var i;
+  do{
+      i = Math.floor(Math.random() * 5);
+  }while(i == current);
+  current = i;
+  console.log(chico[i]);
 }
